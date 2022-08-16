1887b689af272899f03a4143b5e892cf5d27691a
Ohad Cohen <ohad909881122@gmail.com>
2022-08-16 21:23:09 +0300

    U

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -5,7 +5,7 @@ import {
   signInWithEmailAndPassword,
   signOut,
 } from "firebase/auth";
-import { auth } from "C:/Users/PC/Desktop/CryptoApp/my-app/src/firebase-config.js";
+import { auth } from '../../firebase-config';
 
 function Login() {
 
